f09087c10c490369363018c38112b3aebc2249d6
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-07 09:20:52 +0000

    Update vm-to-snapshot.tf

--- vm-to-snapshot.tf
+++ vm-to-snapshot.tf
@@ -1,11 +1,12 @@
 provider "azurerm" {
 subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
-client_id="938340bb-c097-4194-a3fd-f9974b800431"
-client_secret="W**BQBD?qMV0G50xDcXp4PehB:z+QY5y"
+client_id="a7fd1f50-db6d-4503-b952-859d9faef1e9"
+client_secret=":-NXJb/qd66U7OS+3P*T?nUN.CWS:4ow"
 tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df"
 }
+
 resource "azurerm_resource_group" "kp_gp002" {
-  name     = "kp_g002p"
+  name     = "kp_gp002"
   location = "East US"
 }
 
