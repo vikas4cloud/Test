4b6fa71e86c5e20cc567b21590b178df122beb7f
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-08 11:30:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "azurerm_resource_group" "kp_gp01" {
   name     = "kp_gp01"
   location = "eastus"
 }
+resource "azurerm_snapshot" "kp_gp" {
+  name                = "snapshot_VM01"
+  location            = "${azurerm_resource_group.kp_gp.location}"
+  resource_group_name = "${azurerm_resource_group.kp_gp.name}"
+  create_option       = "Copy"
+  source_uri          = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
+}
