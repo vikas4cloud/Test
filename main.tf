ad0b79ca45cd28488c6546afaf70c84afaff5b0b
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-08 11:12:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "azurerm" {
 subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
-client_id="23e85f19-077a-4084-9544-9716a30da2bf"
-client_secret="Fi+ww.5nQ27?AioMW5mfSfYZONA?9uN4"
+client_id="35d39492-2fe9-4ca9-aada-dbc3b879ab80"
+client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
 tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df"
 }
+
 resource "azurerm_resource_group" "kp_gp01" {
   name     = "kp_gp01"
-  location = "East US"
+  location = "eastus"
 }
