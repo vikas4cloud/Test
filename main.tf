f44aae99851217c1b2a88777c09142bd0e1736b7
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-08 11:35:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ resource "azurerm_snapshot" "kp_gp" {
   create_option       = "Copy"
   source_uri          = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
 }
+
+resource "azurerm_managed_disk" "kp_gp" {
+  name = "snapshot-VM02"
+  location = "eastus"
+  resource_group_name = "${azurerm_resource_group.kp_gp.name}"
+  storage_account_type = "Standard_LRS"
+  create_option = "Copy"
+  source_resource_id = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
+  disk_size_gb = "30"
+}
