630a132d925a5d0d1a6b87d1841f58badca126ee
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-09 04:51:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,24 @@ client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
 tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df"
 }
 
-resource "azurerm_resource_group" "kp_gp002" {
-  name     = "kp_gp002"
+resource "azurerm_resource_group" "kp_gp" {
+  name     = "kp_gp"
   location = "eastus"
+}
+resource "azurerm_snapshot" "kp_gp" {
+  name                = "snapshot_VM01"
+  location            = "${azurerm_resource_group.kp_gp.location}"
+  resource_group_name = "${azurerm_resource_group.kp_gp.name}"
+  create_option       = "Copy"
+  source_uri          = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
+}
+
+resource "azurerm_managed_disk" "kp_gp" {
+  name = "snapshot-VM02"
+  location = "eastus"
+  resource_group_name = "${azurerm_resource_group.kp_gp.name}"
+  storage_account_type = "Standard_LRS"
+  create_option = "Copy"
+  source_resource_id = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
+  disk_size_gb = "30"
 }
