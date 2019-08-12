41491ff67399e1f307451fe5d9681e85fd7510d8
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-12 07:06:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 provider "azurerm" {
-subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
-client_id="35d39492-2fe9-4ca9-aada-dbc3b879ab80"
-client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
-tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
 }
 
-resource "azurerm_resource_group" "kp_gp" {
-  name     = "kp_gp"
-  location = "eastus"
+resource "azurerm_resource_group" "rg" {
+ name     = "${var.resource_group_name}"
+ location = "${var.location}"
 }
+
 resource "azurerm_snapshot" "kp_gp" {
   name                = "snapshot_VM01"
   location            = "${azurerm_resource_group.kp_gp.location}"
