5ab5700d710644caac24d41d1c48a6d375c7454e
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 16:39:09 +0000

    Update module.tf

--- module.tf
+++ module.tf
@@ -3,7 +3,7 @@ module "snapshot" {
   subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
   client_id="35d39492-2fe9-4ca9-aada-dbc3b879ab80"
   client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
-  tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df""
+  tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df"
   resource_group_name="kp_gp"
   location="eastus"
 
