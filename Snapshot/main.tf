f5e4edc76b0c51351ebc40ac578ea153a89dbf5a
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 11:55:59 +0000

    Update main.tf

--- Snapshot/main.tf
+++ Snapshot/main.tf
@@ -1,1 +1,20 @@
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+}
+
+resource "azurerm_resource_group" "kp_gp" {
+ name     = "${var.resource_group_name}"
+ location = "${var.location}"
+}
+
+resource "azurerm_snapshot" "kp_gp" {
+  name                = "${var.azurerm_snapshot_name}"
+  location            = "${azurerm_resource_group.kp_gp.location}"
+  resource_group_name = "${azurerm_resource_group.kp_gp.name}"
+  create_option       = "Copy"
+  source_uri          = "${var.source_uri}"
+}
 
