62d8a75022a674f3cdaa519699fa3316bb5cbc00
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 05:15:29 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -34,15 +34,3 @@ variable "location" {
 }
 
 ## snapshot
-ariable "azurerm_snapshot_name" {
- 
- description = "The name of snapshot-os-disk name "
- default     = "sanpshot-os-disk"
-}
-
-variable "create_option" {
-  default = "Copy"
-}
-variable "source_uri" {
-  default = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
-}
