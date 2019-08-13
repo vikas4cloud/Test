46239585ad18aadfa2613c1585d88d698a3f4435
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 12:02:23 +0000

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,13 +1,13 @@
 module "snapshot" {
   source = "./14Aug"
-  subscription_id="59566ed9-5683-4f3e-9064-c7edbfc550ed"
-  client_id="61164566-16a2-489a-b2b1-76922794419c"
-  client_secret="9cI.H1+PEKQ2c.GQ*6hDkFk54qK/P+DT"
-  tenant_id="513294a0-3e20-41b2-a970-6d30bf1546fa"
+  subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
+  client_id="35d39492-2fe9-4ca9-aada-dbc3b879ab80"
+  client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
+  tenant_id="154a555f-9dde-4aed-8873-eca8e68b38df""
   resource_group_name="kp_gp"
   location="eastus"
 
   snapshot_name="snapshot_kumar"
   create_option="Copy"
-  source_uri="subscriptions/59566ed9-5683-4f3e-9064-c7edbfc550ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/vm02_disk1_97f778d2dae942f18e68713444645128"
+  source_uri="subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
 }
