fd5da5c1176107582abe6e7b7841cc5cefbddaa9
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 16:18:43 +0000

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,5 +1,5 @@
 module "snapshot" {
-  source = "./14Aug"
+  source = "./home/jenkins/workspace/terraform/Test/Snapshot"
   subscription_id="3354308b-6548-4749-aac2-6b4cfbf2e6ed"
   client_id="35d39492-2fe9-4ca9-aada-dbc3b879ab80"
   client_secret="0joJRCNWP=+B.e@nH4xsDE27=CSv+58M"
