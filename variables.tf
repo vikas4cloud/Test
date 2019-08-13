6cfebb2dc511e86880c645662bb98568e8f1a6fa
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 05:36:19 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "client_secret" {
 
 variable "resource_group_name" {
  description = "The name of resource group "
- default     = "Welcome001"
+ default     = "kp_gp"
 }
 
 variable "location" {
@@ -34,3 +34,17 @@ variable "location" {
 }
 
 ## snapshot
+
+
+variable "azurerm_snapshot_name" {
+ 
+ description = "The name of snapshot-os-disk name "
+ default     = "sanpshot-os-disk"
+}
+
+variable "create_option" {
+  default = "Copy"
+}
+variable "source_uri" {
+  default = "subscriptions/3354308b-6548-4749-aac2-6b4cfbf2e6ed/resourceGroups/KP_GP/providers/Microsoft.Compute/disks/VM01_disk1_589d699c52ad4851ba177868ca5449c8"
+}
