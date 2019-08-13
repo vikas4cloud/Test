fd51ca4118d1970d1716924f4b9062a3b0f3b40a
kpravin001 <51983884+kpravin001@users.noreply.github.com>
2019-08-13 16:22:45 +0000

    Create variables.tf

---
+++ Snapshot/variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+variable "client_id" {}
+
+variable "client_secret" {}
+
+variable "resource_group_name" {}
+
+variable "location" {}
+
+variable "snapshot_name" {}
+variable "create_option" {}
+variable "source_uri" {}
